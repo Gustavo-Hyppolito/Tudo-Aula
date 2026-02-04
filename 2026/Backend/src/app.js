755f3c6326249c7316822c0d1e874e09b2b0c852
Gustavo-Hyppolito <gustavo.h.rodrigues10@aluno.senai.br>
2026-02-04 12:18:56 -0300

    update app.js

--- 2026/Backend/src/app.js
+++ 2026/Backend/src/app.js
@@ -2,4 +2,31 @@ import express from "express"
 
 const app= express()
 
+app.use(express.json())
+
+const musicas = [{
+    id:133,
+    titulo: "B.Y.O.B",
+    artista: "Sistem of a Down",
+    genero:"POP",
+    ano_publicacao: 1998
+
+
+}]
+
+function buscarMusicas(id){
+    return musicas.findIndex(m => {
+
+        return m.id === Number(id)
+    })
+}
+
+app.get("/musicas", (req, res) =>{
+    res.status(200). json(musicas)
+
+} )
+
+
+
 export default app
+
